e83d49e26c140da94c6e733e60b3ec1ec7dcae29
Thang Mai <maixuanthang@gmail.com>
2019-08-17 15:24:26 +0700

    initial commit

---
+++ apps/love-letterz-e2e/src/integration/app.spec.ts
@@ -0,0 +1,9 @@
+import { getGreeting } from '../support/app.po';
+
+describe('love-letterz', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    getGreeting().contains('Welcome to love-letterz!');
+  });
+});
