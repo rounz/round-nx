ae3c2c792ac2c34aac6fb032c89181c2036824bf
Thang Mai <maixuanthang@gmail.com>
2019-08-18 20:16:31 +0700

    Update CI

--- apps/love-letterz/src/app/app.spec.tsx
+++ apps/love-letterz/src/app/app.spec.tsx
@@ -1,19 +1,35 @@
+import { PlatformVoid, Runtime, ZIO } from '@round/zio'
 import { cleanup, render } from '@testing-library/react'
-import App from './app'
+import AppZ from './app'
 import React from 'react'
+import { pipe } from 'fp-ts/lib/pipeable'
 
 describe('App', () => {
+  const runtime = Runtime.create(void 0, PlatformVoid)
+
   afterEach(cleanup)
 
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />)
+  it('should render successfully', async () => {
+    await pipe(
+      AppZ,
+      ZIO.map(App => {
+        const { baseElement } = render(<App />)
 
-    expect(baseElement).toBeTruthy()
+        expect(baseElement).toBeTruthy()
+      }),
+      runtime.unsafeRun
+    )
   })
 
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />)
+  it('should have a greeting as the title', async () => {
+    await pipe(
+      AppZ,
+      ZIO.map(App => {
+        const { getByText } = render(<App />)
 
-    expect(getByText('Welcome to love-letterz!')).toBeTruthy()
+        expect(getByText('Welcome to love-letterz!')).toBeTruthy()
+      }),
+      runtime.unsafeRun
+    )
   })
 })
